538df02cb47b3f789956fa8699ab5fa33511c265
Oluwasola Ogundare <123597249+OluOgundare@users.noreply.github.com>
2023-08-29 21:34:59 -0400

    Delete Quantanywhere directory

--- Quantanywhere/templates/script.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("loader", function () {
-    const element = document.getElementById("div-01");
-    element.remove(); // Removes the div with the 'div-02' id
-    setTimeout(function () {
-        const element = document.getElementById("div-01");
-        element.remove(); // Removes the div with the 'div-02' id
-      div.classList.remove('loading-page'); // Remove the loading page element from the DOM
-    }, 300);
-  });
